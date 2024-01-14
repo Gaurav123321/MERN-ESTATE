894451770f2c6558e2b7cbcbb3a93f925a0adf25
Student gauravyadav <gauravyadav2003.gy@gmail.com>
2024-01-14 22:47:40 +0530

    create signup Api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 import userRouter from '../api/routes/user.route.js';
+import authRouter from '../api/routes/auth.route.js';
 
 mongoose.connect(process.env.MONGO).then(() =>{
     console.log('Connected to MongoDB!');
@@ -14,8 +15,11 @@ mongoose.connect(process.env.MONGO).then(() =>{
 
 const app=express();
 
+app.use(express.json());
+
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 });
 
 app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
