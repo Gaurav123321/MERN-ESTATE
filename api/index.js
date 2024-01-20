0a31b2185ca544372776eeeb24c389c868dbb264
Student gauravyadav <gauravyadav2003.gy@gmail.com>
2024-01-21 01:48:13 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import listingRouter from '../api/routes/listing.route.js';
 import userRouter from '../api/routes/user.route.js'; 
 import authRouter from '../api/routes/auth.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 mongoose
   .connect(process.env.MONGO)
@@ -15,7 +16,13 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+  const __dirname = path.resolve();
+
+
 const app = express();
+
+
 app.use(express.json());
 app.use(cookieParser());
 app.listen(3000, () => {
@@ -25,6 +32,12 @@ app.listen(3000, () => {
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
+
+app.get('*' ,(req,res)=>{
+  res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
